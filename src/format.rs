594a33778623aab4e095a4db8053f662f1aca44d
Cyprien de Saint Guilhem <cyprien.desaintguilhem@3milabs.tech>
2024-12-21 11:08:18 +0000

    Cargo clippy

--- src/format.rs
+++ src/format.rs
@@ -125,7 +125,7 @@ pub fn format_file(
                     &pattern,
                     queue.front().map(|(_, next_line)| next_line.as_str()),
                     indent_length,
-                    &mut temp_state,
+                    &temp_state,
                     args,
                 ) {
                     // Remove the next line from the queue and replace it after
@@ -188,6 +188,8 @@ pub fn format_file(
     new_text
 }
 
+/// Records into the [`State`] whether the given [`Pattern`] opens or closes
+/// math mode.
 fn check_math_mode(pattern: &Pattern, state: &mut State) {
     if pattern.opens_math_environment && !state.math_mode {
         // We've entered math mode
